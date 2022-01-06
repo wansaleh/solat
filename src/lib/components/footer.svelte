381c74199af122ed379446a47b983051ff561f60
Wan Saleh <wansaleh@gmail.com>
2022-01-06 22:18:03 +0800

    Updates.

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,28 @@
 <footer class="absolute bottom-0 w-full text-sm">
-  <div class="px-4 pb-4 font-semibold tracking-tight text-center text-gray-500">
-    © {new Date().getFullYear()} By Wan Saleh
+  <div
+    class="flex gap-4 justify-center px-4 pb-4 font-semibold tracking-tight text-center text-gray-500"
+  >
+    <a
+      href="https://hadith.wansaleh.com"
+      rel="external"
+      class="hover:border-0 border-b-2 border-current"
+    >
+      Hadith Ringkas
+    </a>
+    <a
+      href="https://hijri.wansaleh.com"
+      rel="external"
+      class="hover:border-0 border-b-2 border-current"
+    >
+      Hijri Holidays
+    </a>
+    <a
+      href="https://panduantarawih.com"
+      rel="external"
+      class="hover:border-0 border-b-2 border-current"
+    >
+      Panduan Tarawih
+    </a>
+    <span>© {new Date().getFullYear()} By Wan Saleh</span>
   </div>
 </footer>
