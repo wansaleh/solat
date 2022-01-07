f441cd3d894209254a93b7bacd7eb40c872074ba
Wan Saleh <wansaleh@gmail.com>
2022-01-07 11:47:32 +0800

    Updates.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,10 @@
 <script>
-  import nprogress from 'nprogress';
-
   import Seo from '$lib/components/seo.svelte';
   import Footer from '$lib/components/footer.svelte';
   import Nav from '$lib/components/nav.svelte';
 
   import '../app.css';
-  import '../nprogress.css';
 </script>
-
-<svelte:window
-  on:sveltekit:navigation-start={() => {
-    nprogress.start();
-  }}
-  on:sveltekit:navigation-end={() => {
-    nprogress.done();
-  }}
-/>
 
 <Seo />
 
@@ -27,8 +15,6 @@
     class="flex relative flex-col justify-center items-center min-h-screen font-normal"
   >
     <slot />
-    <!-- <PageTransition refresh={key}>
-    </PageTransition> -->
 
     <Footer />
   </main>
